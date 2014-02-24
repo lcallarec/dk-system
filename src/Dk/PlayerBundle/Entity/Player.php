4b535f7b456fce41b19aa5a817207303e6f4f145
Laurent Callarec <lcallarec@gmail.com>
2014-02-24 23:44:38 +0100

    Adding Player entity

---
+++ src/Dk/PlayerBundle/Entity/Player.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Dk\PlayerBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Player
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="Dk\PlayerBundle\Entity\PlayerRepository")
+ */
+class Player
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nickname", type="string", length=255)
+     */
+    private $nickname;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set nickname
+     *
+     * @param string $nickname
+     * @return Player
+     */
+    public function setNickname($nickname)
+    {
+        $this->nickname = $nickname;
+
+        return $this;
+    }
+
+    /**
+     * Get nickname
+     *
+     * @return string 
+     */
+    public function getNickname()
+    {
+        return $this->nickname;
+    }
+}
