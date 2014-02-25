e78b1be19fc6175e85b422959c6c25dcf9f87744
Laurent Callarec <lcallarec@gmail.com>
2014-02-25 14:36:36 +0100

    Add Character bundle into kernel

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
             new Dk\PlayerBundle\DkPlayerBundle(),
+            new Dk\CharacterBundle\DkCharacterBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
