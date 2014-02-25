8d5a1f71c787c0b9aeaf465f857fea4254a65df1
Laurent Callarec <lcallarec@gmail.com>
2014-02-25 13:12:33 +0100

    Adding  create, retrieve, update on a Player

--- src/Dk/PlayerBundle/Controller/PlayerController.php
+++ src/Dk/PlayerBundle/Controller/PlayerController.php
@@ -3,8 +3,8 @@
 namespace Dk\PlayerBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Dk\PlayerBundle\Entity\Player;
+use Dk\PlayerBundle\Form\PlayerType;
 
 class PlayerController extends Controller
 {
@@ -15,5 +15,51 @@ class PlayerController extends Controller
         $players = $this->get('doctrine')->getRepository('DkPlayerBundle:Player')->findAll();
         
         return $this->render('DkPlayerBundle:Player:all.html.twig', ['players' => $players]);
+    }
+    
+   /**
+    */
+    public function manageAction($id)
+    {
+        $request = $this->getRequest();
+        
+        if(null !== $id) {
+            $player = $this->get('doctrine')->getRepository('DkPlayerBundle:Player')->findOneById($id);
+        
+            if(!$player) {
+                $this->createNotFoundException("Ce joueur n'existe pas");
+            }
+        } else {
+            $player = new Player();
+        }
+        
+        $form = $this->createForm(new PlayerType(), $player);
+        
+        if($request->getMethod() === 'GET') {
+            
+            return $this->render('DkPlayerBundle:Player:form.html.twig', ['form' => $form->createView()]);
+         
+        } else {
+             
+            $form->handleRequest($request);
+            
+            if($form->isValid()) {
+                
+                $em = $this->get('doctrine')->getManager();
+                
+                $em->persist($player);
+                
+                $em->flush();
+                
+                return $this->render('DkPlayerBundle:Player:all.html.twig');
+                
+            } else {
+                
+                return $this->render('DkPlayerBundle:Player:form.html.twig', ['form' => $form->createView()]);
+            
+            }
+            
+        }
+       
     }
 }
