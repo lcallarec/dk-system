964a2e076c9283a8c2576aeb2d001e1ed8a583c2
laurent <laurent@laurent>
2014-03-03 23:56:41 +0100

    Securtity is now managed by home controller

--- src/Dk/PlayerBundle/Controller/HomeController.php
+++ src/Dk/PlayerBundle/Controller/HomeController.php
@@ -3,11 +3,30 @@ namespace Dk\PlayerBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Security\Core\SecurityContext;
+use Dk\PlayerBundle\Form\RegisterType;
 
 class HomeController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('DkPlayerBundle::home.html.twig');
+        $request = $this->getRequest();
+        $session = $request->getSession();
+        
+        // get the login error if there is one
+        if ($request->attributes->has(SecurityContext::AUTHENTICATION_ERROR)) {
+            $error = $request->attributes->get(SecurityContext::AUTHENTICATION_ERROR);
+        } else {
+            $error = $session->get(SecurityContext::AUTHENTICATION_ERROR);
+            $session->remove(SecurityContext::AUTHENTICATION_ERROR);
+        }
+        
+        $registerForm = $this->createForm(new RegisterType());
+        
+        return $this->render('DkPlayerBundle::home.html.twig', [
+            'error'         => $error,
+            'last_username' => $session->get(SecurityContext::LAST_USERNAME),
+            'register_form' => $registerForm->createView()
+        ]);
     }
+  
 }
