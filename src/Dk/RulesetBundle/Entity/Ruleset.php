8f9786d04ce751a9335e4433db847f85595ef952
laurent <laurent@laurent>
2014-03-03 20:46:14 +0100

    Adding base ruleset entity / repository

---
+++ src/Dk/RulesetBundle/Entity/Ruleset.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Dk\RulesetBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Ruleset
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="Dk\RulesetBundle\Entity\RulesetRepository")
+ */
+class Ruleset
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Ruleset
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+}
