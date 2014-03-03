7ea8b4d12790bddf57605e0d5bfcf2652ab4ddcb
laurent <laurent@laurent>
2014-03-03 16:46:36 +0100

    Adding CampaignBundle to kernel

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
             new Dk\PlayerBundle\DkPlayerBundle(),
             new Dk\CharacterBundle\DkCharacterBundle(),
+            new Dk\CampaignBundle\DkCampaignBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
