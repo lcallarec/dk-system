230763502c10a9876e74388efdf3648d00b2296a
laurent <laurent@laurent>
2014-03-04 16:04:17 +0100

    Add findPlayerCharacters method

--- src/Dk/Bundle/SystemBundle/Repository/PlayerCharacterRepository.php
+++ src/Dk/Bundle/SystemBundle/Repository/PlayerCharacterRepository.php
@@ -3,6 +3,7 @@
 namespace Dk\Bundle\SystemBundle\Repository;
 
 use Doctrine\ORM\EntityRepository;
+use Dk\Bundle\SystemBundle\Entity\Player;
 
 /**
  * PlayerCharacterRepository
@@ -12,4 +13,22 @@ use Doctrine\ORM\EntityRepository;
  */
 class PlayerCharacterRepository extends EntityRepository
 {
+    public function findPlayerCharacters(Player $player)
+    {
+        $query = $this->getEntityManager()
+            ->createQuery('
+                SELECT p, pc, c FROM DkSystemBundle:PlayerCharacter pc
+                LEFT JOIN pc.player p
+                LEFT JOIN pc.campaign c
+                WHERE p = :player
+                ORDER BY c.name'
+            )
+            ->setParameter(':player', $player);
+
+        try {
+            return $query->getResult();
+        } catch (\Doctrine\ORM\NoResultException $e) {
+            return null;
+        }
+    }
 }
