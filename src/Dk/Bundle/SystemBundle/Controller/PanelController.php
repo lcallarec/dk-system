69c0251ef53f01027c225ffbe50d4b43f4b4bf69
lcallarec <l.callarec@gmail.com>
2014-03-04 15:40:08 +0100

    Change player panel organization

--- src/Dk/Bundle/SystemBundle/Controller/PanelController.php
+++ src/Dk/Bundle/SystemBundle/Controller/PanelController.php
@@ -3,8 +3,6 @@
 namespace Dk\Bundle\SystemBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Dk\Bundle\SystemBundle\Entity\Player;
-use Dk\Bundle\SystemBundle\Form\PlayerType;
 
 class PanelController extends Controller
 {
@@ -12,7 +10,13 @@ class PanelController extends Controller
      */
     public function indexAction()
     {
-        return $this->render('DkSystemBundle:Panel:index.html.twig');
+          
+        $pcs = $this->get('doctrine')
+                ->getRepository('DkSystemBundle:PlayerCharacter')
+                ->findPlayerCharacters($this->getUser())
+        ;
+        
+        return $this->render('DkSystemBundle:Panel:panel.html.twig', ['pcs' => $pcs]);
     }
 
 }
