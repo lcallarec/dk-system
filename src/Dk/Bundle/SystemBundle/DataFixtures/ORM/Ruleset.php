95cc1eb21807effd35308eef89615eec713dd869
lcallarec <l.callarec@gmail.com>
2014-03-05 12:28:56 +0100

    Adding ruleset fixtures

---
+++ src/Dk/Bundle/SystemBundle/DataFixtures/ORM/Ruleset.php
@@ -0,0 +1,37 @@
+<?php
+namespace Dk\Bundle\SystemBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+
+use Dk\Bundle\SystemBundle\Entity\Ruleset;
+
+class LoadRulesetData extends AbstractFixture implements FixtureInterface, OrderedFixtureInterface
+{
+    
+    /**
+     * {@inheritDoc}
+     */
+    public function load(ObjectManager $manager)
+    {
+        $ruleset = new Ruleset();
+        $ruleset->setName('dk2 standard edition');
+        
+        $this->addReference('ruleset-1', $ruleset);
+        
+        $manager->persist($ruleset);
+        $manager->flush();
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    public function getOrder()
+    {
+        return 2;
+    }
+
+    
+}
