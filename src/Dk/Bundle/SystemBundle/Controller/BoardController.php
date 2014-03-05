883b4f4bb3f494fa535b56d89085a06d75872848
lcallarec <l.callarec@gmail.com>
2014-03-05 11:11:17 +0100

    Retrieve datas according to granted roles

--- src/Dk/Bundle/SystemBundle/Controller/BoardController.php
+++ src/Dk/Bundle/SystemBundle/Controller/BoardController.php
@@ -19,10 +19,21 @@ class BoardController extends Controller
      */
     public function menuAction()
     {
-         $pcs = $this->get('doctrine')
+        if($this->get('security.context')->isGranted('ROLE_PLAYER')) {
+            $pcs = $this->get('doctrine')
                 ->getRepository('DkSystemBundle:PlayerCharacter')
                 ->findPlayerCharacters($this->getUser())
-        ;
+            ;  
+        } else {
+            $pcs = null;
+        }
+        
+        if($this->get('security.context')->isGranted('ROLE_MASTER')) {
+           
+        } else {
+    
+        }
+         
          
         return $this->render('DkSystemBundle:Board:menu.html.twig', ['pcs' => $pcs]);
     }
