5dbed40eb9f8f5502a01484063bfb4a8a870eeaf
laurent <laurent@laurent>
2014-03-06 12:06:50 +0100

    Add type

---
+++ src/Dk/Bundle/SystemBundle/Form/Type/PlayerCharacterCharacteristicType.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Dk\Bundle\SystemBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\Form\FormBuilderInterface;
+
+use Dk\Bundle\SystemBundle\Form\DataTransformer\ChoiceToTextTransformer;
+
+
+class PlayerCharacterCharacteristicType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('value')
+            ->add()
+        ;
+        
+         $builder->add(
+                $builder
+                    ->create('characteristic', 'plain_text', [
+                           
+                     ])
+                    ->addModelTransformer(new ChoiceToTextTransformer())
+            );
+    }
+
+   /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Dk\Bundle\SystemBundle\Entity\PlayerCharacterCharacteristic'
+        ));
+    }    
+    
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'char';
+    }
+}
