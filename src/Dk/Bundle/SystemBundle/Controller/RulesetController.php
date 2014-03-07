cfd6c642764c17f16928b0d7664f0f8b39be2b57
laurent <laurent@laurent>
2014-03-07 21:20:19 +0100

    Manage playable races

--- src/Dk/Bundle/SystemBundle/Controller/RulesetController.php
+++ src/Dk/Bundle/SystemBundle/Controller/RulesetController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 use Dk\Bundle\SystemBundle\Form\RulesetType;
 use Dk\Bundle\SystemBundle\Form\RulesetSkillCollectionType;
+use \Dk\Bundle\SystemBundle\Form\RulesetPlayableRaceCollectionType;
 
 class RulesetController extends Controller
 {
@@ -102,50 +103,52 @@ class RulesetController extends Controller
        
     }
     
-//   /**
-//    * Manage Playable races skills
-//    */
-//    public function managePlayableRacesAction($id)
-//    {
-//        $request = $this->getRequest();
-//        
-//        //A ruleset can't be created this way yet
-//        if(null !== $id) {
-//            $races = $this->get('doctrine')->getRepository('DkSystemBundle:Ruleset')->findOneById($id);
-//        }
-//
-//        if(null === $id) {
-//            throw $this->createNotFoundException("Ce système de règles n'existe pas ou n'existe plus. Aucune coméptence à gérer.");
-//        }
-//
-//        $em = $this->get('doctrine')->getManager();
-//        
-//        $form = $this->createForm(new RulesetSkillCollectionType(), $skills);
-//        
-//        if($request->getMethod() === 'GET') {
-// 
-//            return $this->render('DkSystemBundle:Ruleset:Skill/form.html.twig', ['form' => $form->createView()]);
-//         
-//        } else {
-//    
-//            $form->handleRequest($request);
-//            
-//            if($form->isValid()) {
-//    
-//                $em->persist($skills);
-//                
-//                $em->flush();
-//                
-//                return $this->redirect($this->generateUrl('manage_ruleset', ['id' => $id]));
-//                
-//            } else {
-//
-//                return $this->render('DkSystemBundle:Ruleset:Skill/form.html.twig', ['form' => $form->createView()]);
-//            
-//            }
-//            
-//        }
-//       
-//    }    
+   /**
+    * Manage Playable races skills
+    */
+    public function managePlayableRacesAction($id)
+    {
+        $request = $this->getRequest();
+        
+        if(null !== $id) {
+            $ruleset = $this->get('doctrine')
+                          ->getRepository('DkSystemBundle:Ruleset')
+                          ->findOneWithPlayableRaces($this->getUser(), $id)
+            ;
+        }
+
+        if(null === $id) {
+            throw $this->createNotFoundException("Ce système de règles n'existe pas ou n'existe plus. Aucune race jouable à gérer.");
+        }
+
+        $em = $this->get('doctrine')->getManager();
+        
+        $form = $this->createForm(new RulesetPlayableRaceCollectionType(), $ruleset);
+        
+        if($request->getMethod() === 'GET') {
+ 
+            return $this->render('DkSystemBundle:Ruleset:PlayableRace/form.html.twig', ['form' => $form->createView()]);
+         
+        } else {
+    
+            $form->handleRequest($request);
+            
+            if($form->isValid()) {
+    
+                $em->persist($ruleset);
+                
+                $em->flush();
+                
+                return $this->redirect($this->generateUrl('manage_ruleset', ['id' => $id]));
+                
+            } else {
+
+                return $this->render('DkSystemBundle:Ruleset:PlayableRace/form.html.twig', ['form' => $form->createView()]);
+            
+            }
+            
+        }
+       
+    }    
     
 }
