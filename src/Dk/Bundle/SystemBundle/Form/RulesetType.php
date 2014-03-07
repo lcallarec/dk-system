6ce7707e607219b9b820a8f0bc78b76869ece9cc
lcallarec <l.callarec@gmail.com>
2014-03-07 15:17:20 +0100

    Add the Ruleset Type

---
+++ src/Dk/Bundle/SystemBundle/Form/RulesetType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Dk\Bundle\SystemBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class RulesetType extends AbstractType
+{
+        /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('owner')
+            
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Dk\Bundle\SystemBundle\Entity\Ruleset'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'ruleset';
+    }
+}
