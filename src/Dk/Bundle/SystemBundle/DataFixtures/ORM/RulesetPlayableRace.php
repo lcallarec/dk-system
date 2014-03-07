ddadaf90c6cad52bef4eb0b1e0951bdb1980212d
lcallarec <l.callarec@gmail.com>
2014-03-07 10:12:56 +0100

    Adding fixtures

---
+++ src/Dk/Bundle/SystemBundle/DataFixtures/ORM/RulesetPlayableRace.php
@@ -0,0 +1,67 @@
+<?php
+namespace Dk\Bundle\SystemBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+
+use Dk\Bundle\SystemBundle\Entity\RulesetPlayableRace;
+
+class LoadRulesetPlayableRaceData extends AbstractFixture implements FixtureInterface, OrderedFixtureInterface
+{
+    
+    /**
+     * {@inheritDoc}
+     */
+    public function load(ObjectManager $manager)
+    {
+        
+        $races = [
+          ['name' => 'Humain'], ['name' => 'Gros nain qui pue']  
+        ];
+        
+        foreach($races as $r) {
+            
+            $race = new RulesetPlayableRace();
+            
+            $race
+                    ->setName($r['name'])
+                    ->setRuleset($this->getReference('ruleset-1'))
+            ;
+            
+            $manager->persist($race);
+           
+        }
+        
+        $races = [
+          ['name' => 'Humain custom'], ['name' => 'Gros nain qui pue custom']  
+        ];
+        
+        foreach($races as $r) {
+            
+            $race = new RulesetPlayableRace();
+            
+            $race
+                    ->setName($r['name'])
+                    ->setRuleset($this->getReference('ruleset-2'))
+            ;
+            
+            $manager->persist($race);
+           
+        }
+        
+        $manager->flush();
+  
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    public function getOrder()
+    {
+        return 6;
+    }
+
+    
+}
