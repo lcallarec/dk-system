a9f53aacf55dc6e2679a1da0950dcedf3c3f48ab
lcallarec <l.callarec@gmail.com>
2014-03-07 21:01:43 +0100

    Use findOneWithSkills on skills management

--- src/Dk/Bundle/SystemBundle/Controller/RulesetController.php
+++ src/Dk/Bundle/SystemBundle/Controller/RulesetController.php
@@ -56,7 +56,7 @@ class RulesetController extends Controller
        
     }
     
-     /**
+   /**
     * Manage Ruleset skills
     */
     public function manageSkillsAction($id)
@@ -65,7 +65,7 @@ class RulesetController extends Controller
         
         //A ruleset can't be created this way yet
         if(null !== $id) {
-            $skills = $this->get('doctrine')->getRepository('DkSystemBundle:Ruleset')->findOneById($id);
+            $skills = $this->get('doctrine')->getRepository('DkSystemBundle:Ruleset')->findOneWithSkills($this->getUser(), $id);
         }
 
         if(null === $id) {
@@ -90,15 +90,62 @@ class RulesetController extends Controller
                 
                 $em->flush();
                 
-                return $this->redirect($this->generateUrl('board'));
+                return $this->redirect($this->generateUrl('manage_ruleset', ['id' => $id]));
                 
             } else {
 
-                //return $this->render('DkSystemBundle:PlayerCharacter:form.html.twig', ['form' => $form->createView(), 'pc' => $pc]);
+                return $this->render('DkSystemBundle:Ruleset:Skill/form.html.twig', ['form' => $form->createView()]);
             
             }
             
         }
        
     }
+    
+//   /**
+//    * Manage Playable races skills
+//    */
+//    public function managePlayableRacesAction($id)
+//    {
+//        $request = $this->getRequest();
+//        
+//        //A ruleset can't be created this way yet
+//        if(null !== $id) {
+//            $races = $this->get('doctrine')->getRepository('DkSystemBundle:Ruleset')->findOneById($id);
+//        }
+//
+//        if(null === $id) {
+//            throw $this->createNotFoundException("Ce système de règles n'existe pas ou n'existe plus. Aucune coméptence à gérer.");
+//        }
+//
+//        $em = $this->get('doctrine')->getManager();
+//        
+//        $form = $this->createForm(new RulesetSkillCollectionType(), $skills);
+//        
+//        if($request->getMethod() === 'GET') {
+// 
+//            return $this->render('DkSystemBundle:Ruleset:Skill/form.html.twig', ['form' => $form->createView()]);
+//         
+//        } else {
+//    
+//            $form->handleRequest($request);
+//            
+//            if($form->isValid()) {
+//    
+//                $em->persist($skills);
+//                
+//                $em->flush();
+//                
+//                return $this->redirect($this->generateUrl('manage_ruleset', ['id' => $id]));
+//                
+//            } else {
+//
+//                return $this->render('DkSystemBundle:Ruleset:Skill/form.html.twig', ['form' => $form->createView()]);
+//            
+//            }
+//            
+//        }
+//       
+//    }    
+    
 }
