60c22ab52d3cc1c0411e373e00e46de0f0e9882d
lcallarec <l.callarec@gmail.com>
2014-03-07 21:37:11 +0100

    Add rules for managing assets

--- src/Dk/Bundle/SystemBundle/Controller/RulesetController.php
+++ src/Dk/Bundle/SystemBundle/Controller/RulesetController.php
@@ -6,7 +6,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 use Dk\Bundle\SystemBundle\Form\RulesetType;
 use Dk\Bundle\SystemBundle\Form\RulesetSkillCollectionType;
-use \Dk\Bundle\SystemBundle\Form\RulesetPlayableRaceCollectionType;
+use Dk\Bundle\SystemBundle\Form\RulesetPlayableRaceCollectionType;
+use Dk\Bundle\SystemBundle\Form\RulesetAssetCollectionType;
 
 class RulesetController extends Controller
 {
@@ -64,7 +65,6 @@ class RulesetController extends Controller
     {
         $request = $this->getRequest();
         
-        //A ruleset can't be created this way yet
         if(null !== $id) {
             $skills = $this->get('doctrine')->getRepository('DkSystemBundle:Ruleset')->findOneWithSkills($this->getUser(), $id);
         }
@@ -149,6 +149,54 @@ class RulesetController extends Controller
             
         }
        
-    }    
+    }
+    
+ /**
+    * Manage Ruleset assets
+    */
+    public function manageAssetsAction($id)
+    {
+        $request = $this->getRequest();
+        
+        if(null !== $id) {
+            $ruleset = $this->get('doctrine')
+                           ->getRepository('DkSystemBundle:Ruleset')
+                           ->findOneWithAssets($this->getUser(), $id);
+        }
+
+        if(null === $id) {
+            throw $this->createNotFoundException("Ce système de règles n'existe pas ou n'existe plus. Aucun atouts à gérer.");
+        }
+
+        $em = $this->get('doctrine')->getManager();
+        
+        $form = $this->createForm(new RulesetAssetCollectionType(), $ruleset);
+        
+        if($request->getMethod() === 'GET') {
+ 
+            return $this->render('DkSystemBundle:Ruleset:Asset/form.html.twig', ['form' => $form->createView()]);
+         
+        } else {
+    
+            $form->handleRequest($request);
+            
+            if($form->isValid()) {
+    
+                $em->persist($ruleset);
+                
+                $em->flush();
+                
+                return $this->redirect($this->generateUrl('manage_ruleset', ['id' => $id]));
+                
+            } else {
+
+                return $this->render('DkSystemBundle:Ruleset:Asset/form.html.twig', ['form' => $form->createView()]);
+            
+            }
+            
+        }
+       
+    }
+        
     
 }
