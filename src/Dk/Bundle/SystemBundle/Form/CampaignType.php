f8a71160d9720301b115c1c2e43f13e460ac7980
lcallarec <l.callarec@gmail.com>
2014-04-14 23:07:15 +0200

    Fix first step of PC creation when master don't have set a campaign for this PC yet

--- src/Dk/Bundle/SystemBundle/Form/CampaignType.php
+++ src/Dk/Bundle/SystemBundle/Form/CampaignType.php
@@ -5,8 +5,6 @@ namespace Dk\Bundle\SystemBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
-
-use Symfony\Component\Form\Extension\Core\DataTransformer\ChoiceToValueTransformer;
 use Dk\Bundle\SystemBundle\Repository\PlayerCharacterRepository;
 
 class CampaignType extends AbstractType
@@ -18,7 +16,9 @@ class CampaignType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
+            ->add('name', 'text', [
+                'label' => 'campaign.name'
+            ])
             ->add('playerCharacters', 'entity', 
                 [
                     'class'     => 'DkSystemBundle:PlayerCharacter',
@@ -29,7 +29,7 @@ class CampaignType extends AbstractType
                             return $pr->createQueryBuilder('pc')
                                 ->where('pc.campaign IS NULL')
                                 ->orderBy('pc.firstname', 'ASC');
-                    },                    
+                    },
                 ])
             ->add('submit', 'submit')
         ;
