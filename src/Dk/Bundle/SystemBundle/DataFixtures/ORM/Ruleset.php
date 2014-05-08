84b7730ec905b4c9634aac4780eafe56d5aa64f5
lcallarec <l.callarec@gmail.com>
2014-05-08 22:12:42 +0200

    Add a ruleset to each skillgroups

--- src/Dk/Bundle/SystemBundle/DataFixtures/ORM/Ruleset.php
+++ src/Dk/Bundle/SystemBundle/DataFixtures/ORM/Ruleset.php
@@ -111,7 +111,7 @@ class LoadRulesetData extends AbstractFixture implements FixtureInterface, Order
             $group = new RulesetSkillGroup();
             $group
                 ->setName($name)
-                //->setRuleset($ruleset)
+                ->setRuleset($ruleset)
             ;
 
             if (1 !== $level) {
