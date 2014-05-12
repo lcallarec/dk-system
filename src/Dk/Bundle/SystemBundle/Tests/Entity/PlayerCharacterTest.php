6c16de69aee80a171455a3c92efea70b928895cc
lcallarec <l.callarec@gmail.com>
2014-05-13 00:04:52 +0200

    Repair test

--- src/Dk/Bundle/SystemBundle/Tests/Entity/PlayerCharacterTest.php
+++ src/Dk/Bundle/SystemBundle/Tests/Entity/PlayerCharacterTest.php
@@ -17,26 +17,28 @@ class PlayerCharacterTest extends \PHPUnit_Framework_TestCase
      */
     private $player;    
     
-    public function setUp() {
-        
-        $this->pc = new PlayerCharacter();
+    public function setUp()
+    {
+        $this->player = new Player();
+        $this->pc = new PlayerCharacter($this->player);
         $this->pc->setFirstname('Lamache');
         $this->pc->setLastname('Gordillo');
-        
-        $this->player = new Player();
-        
+
         $this->pc->setPlayer($this->player);
     }
     
-    public function testGetFirstname() {
+    public function testGetFirstname()
+    {
         $this->assertEquals('Lamache', $this->pc->getFirstname(), "Le prénom du personnage est juste");
     }
     
-    public function testGetLastname() {
+    public function testGetLastname()
+    {
         $this->assertEquals('Gordillo', $this->pc->getLastname(), "Le nom du personnage est juste");
     }
     
-    public function testGetPlayer() {
+    public function testGetPlayer()
+    {
         $this->assertInstanceOf('Dk\Bundle\SystemBundle\Entity\Player', $this->pc->getPlayer(), "Le joueur est bien une instance de Player");
     }
 }
