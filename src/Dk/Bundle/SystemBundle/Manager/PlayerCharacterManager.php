fc0ffb0a67282b37f1284269071f3dfe938f1988
lcallarec <l.callarec@gmail.com>
2014-05-24 16:49:50 +0200

    Add some proxy methods and inject Player in constructor

--- src/Dk/Bundle/SystemBundle/Manager/PlayerCharacterManager.php
+++ src/Dk/Bundle/SystemBundle/Manager/PlayerCharacterManager.php
@@ -8,6 +8,7 @@ use Doctrine\ORM\EntityRepository;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Dk\Bundle\SystemBundle\Entity\PlayerCharacter;
 use Dk\Bundle\SystemBundle\Events\PlayerCharacterEvent;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * Class PlayerCharacterManager
@@ -25,16 +26,40 @@ class PlayerCharacterManager implements ManagerInterface
     /** @var EventDispatcherInterface */
     private $eventDispatcher;
 
+    /** @var Player */
+    private $player;
+
     /**
+     * @param UserInterface            $player
      * @param EntityManager            $em
      * @param EventDispatcherInterface $eventDispatcher
      */
-    public function __construct(EntityManager $em, EventDispatcherInterface $eventDispatcher)
+    public function __construct(UserInterface $player, EntityManager $em, EventDispatcherInterface $eventDispatcher)
     {
+        $this->player          = $player;
         $this->em              = $em;
         $this->repository      = $em->getRepository('DkSystemBundle:PlayerCharacter');
         $this->eventDispatcher = $eventDispatcher;
     }
+
+    /**
+     * @param $id
+     * @return PlayerCharacter
+     */
+    public function get($id)
+    {
+        return $this->getRepository()->findOneById($id);
+    }
+
+    /**
+     * @param $id
+     * @return PlayerCharacter
+     */
+    public function getWithRelationships($id)
+    {
+        return $this->getRepository()->findOneWithRelationships($this->player, $id);
+    }
+
 
     /**
      * @return EntityRepository
