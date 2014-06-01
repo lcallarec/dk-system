d6e19864ab1fdaf69cc360a89ac5116dd08d7354
lcallarec <l.callarec@gmail.com>
2014-06-01 22:53:44 +0200

    Add import bundle to kernel

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
             new Dk\Bundle\SystemBundle\DkSystemBundle(),
+            new Dk\Bundle\ImportBundle\DkImportBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
