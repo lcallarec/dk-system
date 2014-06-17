eebd42fa1660084a199de9f4d9af04451f81ed58
lcallarec <l.callarec@gmail.com>
2014-06-17 23:17:41 +0200

    Add Ruleset\AssetConfig management

---
+++ src/Dk/Bundle/SystemBundle/Entity/Ruleset/AssetConfig.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Dk\Bundle\SystemBundle\Entity\Ruleset;
+
+/**
+ * Class AssetConfig
+ *
+ * @package Dk\Bundle\SystemBundle\Entity\Ruleset
+ *
+ * @author Laurent Callarec <l.callarec@gmail.com>
+ */
+class AssetConfig extends Config
+{
+
+}
